cd46cf868d93fbff7a0de385bd1075355deeb160
Carl Andersson <carl.andersson@xenit.se>
2021-11-15 15:03:56 +0100

    fix for azure-cli

--- docker/install-scripts/azure-cli.sh
+++ docker/install-scripts/azure-cli.sh
@@ -13,9 +13,6 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-az extension add --name azure-devops
-az extension add --name managementpartner
-
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
     tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
@@ -24,4 +21,8 @@ curl -sL https://packages.microsoft.com/keys/microsoft.asc |
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
     tee /etc/apt/sources.list.d/azure-cli.list
 
+  apt update
   apt-get install azure-cli=${VERSION}-1~$AZ_REPO
+
+az extension add --name azure-devops
+az extension add --name managementpartner
